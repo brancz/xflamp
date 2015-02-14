64afb434781f01be91a1cdd16aeeb16967c3ea08
flower-pot <flower-pot@flowerpot.io>
2015-02-14 15:16:35 +0100

    do hard reload of repo

--- lib/xflamp/travis.rb
+++ lib/xflamp/travis.rb
@@ -1,31 +1,31 @@
-require 'travis'
+require 'travis/client/session'
 require 'xflamp/aggregated_green'
 
 module XFLamp
   module TravisCI
     class Project
-      attr_reader :repo_slug
+      attr_reader :repo, :session
 
-      def initialize(repo_slug)
-        @repo_slug = repo_slug
+      def initialize(repo_slug, server)
+        @session = server.session
+        @repo = session.find_one(Travis::Client::Repository, repo_slug)
       end
 
       def green?
-        repo = Travis::Repository.find(repo_slug)
-        green = repo.last_build.green?
-        repo.caches.each(&:delete)
-        green
+        session.reload(repo)
+        repo.last_build.green?
       end
     end
 
     class Org
       include AggregatedGreen
 
-      attr_reader :projects
+      attr_reader :projects, :session
       alias_method :targets, :projects
 
       def initialize(config)
-        @projects = config['projects'].map {|repo_slug| Project.new repo_slug }
+        @session = Travis::Client::Session.new
+        @projects = config['projects'].map {|repo_slug| Project.new repo_slug, self }
       end
     end
   end

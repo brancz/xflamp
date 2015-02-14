5340fb9e53343624a649c7d032e0f2b9286592ff
flower-pot <flower-pot@flowerpot.io>
2015-02-14 14:23:20 +0100

    fix on and off gpio writing

--- lib/xflamp/lamp.rb
+++ lib/xflamp/lamp.rb
@@ -2,18 +2,20 @@ require 'wiringpi'
 
 module XFLamp
   class Lamp
+    attr_reader :pin, :gpio
+
     def initialize(pin = 0)
       @pin = pin
       @gpio = WiringPi::GPIO.new
-      @gpio.mode pin, OUTPUT
+      gpio.mode pin, OUTPUT
     end
 
     def on!
-      @gpio.write pin, 0
+      gpio.write pin, 1
     end
 
     def off!
-      @gpio.write pin, 0
+      gpio.write pin, 0
     end
   end
 end
